f0af3d20e8569e663860d74f793e0348bc434a51
Marteen21 <znmarteen21@gmail.com>
2016-01-09 22:31:03 +0100

    Added a few offsets

--- BlackMagicPractice/Program.cs
+++ BlackMagicPractice/Program.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static BotTemplate.Constants.Offsets;
 
 namespace BlackMagicPractice {
     class Program {
@@ -17,16 +18,54 @@ namespace BlackMagicPractice {
             uint plvl2 = wow.ReadUInt(plvl1 + 0x9c);
             uint plvl3 = wow.ReadUInt(plvl2 + 0x5c);
             uint plvl4 = wow.ReadUInt(plvl3 + 0x60);
+            uint player_guid = plvl3 - 0x10;
             //uint playerbase = wow.ReadUInt(wow.ReadUInt(wow.ReadUInt(0x0A87EC2C+0x40))); //this is the player base
             string playername = wow.ReadASCIIString(0x00DBE820, 256); //reads player name
-            Console.WriteLine(playername + "'s max HP is: " +plvl4);
-            for(uint i=0;i<500;i++) {
-                uint temp = wow.ReadUInt(plvl4 + (0x10 * i));
-                if (temp != 0) {
-                    Console.WriteLine("Value: " + temp + " at 0x{0:X}",(0x10*i));
+            Console.WriteLine(wow.ReadUInt(player_guid+(uint)descriptors.Health));
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            /*Console.WriteLine(playername + plvl4);
+                for (uint j = 0; j < 1; j++) {
+                try {
+                    uint c = uint.Parse(Console.ReadLine());
+
+                    for (uint i = 0; i < 2000; i++) {
+                        uint temp = wow.ReadUInt(plvl3 + (0x10 * i));
+                        if (temp > 0 && temp < 200000) {
+                            Console.WriteLine("Value: " + temp + " at 0x{0:X}", (0x10 * i));
+                            if (temp == c) {
+                                Console.WriteLine("Found it! Press enter to continue...");
+                                Console.ReadLine();
+                            }
+                        }
+                    }
+                }
+                catch {
+
                 }
             }
+            for (uint i = 0; i < 100; i++) {
+                try {
+                    uint offset = uint.Parse(Console.ReadLine());
+                    Console.WriteLine(wow.ReadUInt(plvl3 + offset));
+                }
+                catch {
 
+                }
+            }
+            */
             //////////////////
             Console.ReadLine();
         }
